c6239b254801d071051bc31fd9d60efd0c807822
v.shatalov <v.shatalov@disoft.dev>
2023-04-18 12:20:56 +0300

    Upload to PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import django_history
 
 setup(
-    name='django_history',
+    name='django_histories',
     version=django_history.__version__,
     author='QuantumaStelata',
     author_email='quantumastelata@gmail.com',
