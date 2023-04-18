c27c452c02afd680bdd14508e02cee74afa5a5ce
Vladyslav <62384889+QuantumaStelata@users.noreply.github.com>
2023-04-18 11:48:04 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import django_history
 
 setup(
-    name='django-history',
+    name='django_history',
     version=django_history.__version__,
     author='QuantumaStelata',
     author_email='quantumastelata@gmail.com',
