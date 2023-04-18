88d9972d50e48faaa78eb8f0d73478b898acdfa2
v.shatalov <v.shatalov@disoft.dev>
2023-04-18 11:16:16 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+import history
+
+setup(
+    name='django-history',
+    version=history.___version__,
+    author='QuantumaStelata',
+    author_email='quantumastelata@gmail.com',
+)
