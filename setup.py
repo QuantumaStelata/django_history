68edf445d1dff9086f95dde95c5e5fd4a878cf5b
v.shatalov <v.shatalov@disoft.dev>
2023-04-18 11:20:42 +0300

    Fix __version__

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import django_history
 
 setup(
     name='django-history',
-    version=django_history.___version__,
+    version=django_history.__version__,
     author='QuantumaStelata',
     author_email='quantumastelata@gmail.com',
 )
